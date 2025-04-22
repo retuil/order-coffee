08e542c2127962d618eab7a0228566122c3d8ce5
usiqwerty <benlogin1@protonmail.com>
2025-04-22 16:21:45 +0500

    task 2

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
+let lastDrinkNumber = 1;
 let formCount = 1;
 
 function addNewForm() {
+    lastDrinkNumber++;
     formCount++;
     const form = document.querySelector('form');
     const newForm = form.cloneNode(true);
-    newForm.querySelector('.beverage-count').textContent = `Напиток № ${formCount}`;
+    newForm.querySelector('.beverage-count').textContent = `Напиток № ${lastDrinkNumber}`;
 
     newForm.reset();
     newForm.querySelector('.add-button').addEventListener('click', addNewForm);
+    newForm.querySelector('.delete-button').addEventListener('click', deleteForm);
     form.parentNode.appendChild(newForm);
 
 }
 
+function deleteForm(e) {
+    e.preventDefault();
+    if (formCount === 1) {
+        return;
+    }
+    formCount--;
+    e.target.parentNode.parentNode.parentNode.remove();
+}
+
 document.querySelector('.add-button').addEventListener('click', addNewForm);
+document.querySelector('.delete-button').addEventListener('click', deleteForm);
