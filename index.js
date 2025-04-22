7f31975798fb3ed4856368d05b734fcd6f7b56b5
usiqwerty <benlogin1@protonmail.com>
2025-04-22 16:36:30 +0500

    task 5

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 let lastDrinkNumber = 1;
 let formCount = 1;
 
+function numberStringForm(n) {
+    if (n % 10 === 1 && n % 100 !== 11) {
+        return 'напиток'
+    }
+    if ((n % 100 !== 12 && n % 100 !== 13 && n % 100 !== 14) &&
+        (n % 10 === 2 || n % 10 === 3 || n % 10 === 4)) {
+        return 'напитка';
+    }
+    return 'напитков';
+}
+
 function addNewForm() {
     lastDrinkNumber++;
     formCount++;
@@ -11,6 +22,7 @@ function addNewForm() {
     newForm.reset();
     newForm.querySelector('.add-button').addEventListener('click', addNewForm);
     newForm.querySelector('.delete-button').addEventListener('click', deleteForm);
+    newForm.querySelector('.submit-button').addEventListener("click", processingSubmit);
     form.parentNode.appendChild(newForm);
 
 }
@@ -27,15 +39,18 @@ function deleteForm(e) {
 document.querySelector('.add-button').addEventListener('click', addNewForm);
 document.querySelector('.delete-button').addEventListener('click', deleteForm);
 
-function processingSubmit(event){
-    document.querySelector('.modal-overlay').style.display = "block";
+function processingSubmit(event) {
+    const modal = document.querySelector('.modal-overlay');
+    modal.querySelector('h2').textContent = `Вы заказали ${formCount} ${numberStringForm(formCount)}`;
+    modal.style.display = "block";
     event.preventDefault();
 }
 
-function closeModal(event){
+function closeModal(event) {
     document.querySelector('.modal-overlay').style.display = "none";
 }
-for (const submitButton of document.getElementsByClassName("submit-button")){
+
+for (const submitButton of document.getElementsByClassName("submit-button")) {
     submitButton.addEventListener("click", processingSubmit);
 }
 
